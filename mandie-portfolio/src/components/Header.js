575320296ed506c74cebe2a57b6ed4a8ebd7ba0d
MandieBot <amandabotinelly@gmail.com>
2023-01-13 09:56:05 -0700

    Modifying header with profile photo, created images folder

--- mandie-portfolio/src/components/Header.js
+++ mandie-portfolio/src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
+import profilepic from "../images/profilepic.jpeg";
 
 function Header() {
   const linkStyle = { border: "1px black", padding: "5px" };
 
   return (
     <div className="header">
-      {/* <img src="./assets/images/headshot1.jpeg" alt="headshot image" /> */}
+      <img class ="headerImg" src={profilepic} alt="headshot image" />
       <h1>Mandie Botinelly</h1>
       <h3>
         <strong>A Gallery of Past & Ongoing Projects</strong>
